705cd0bf91fb56d461e63c668d78ce0083f95839
NutWatcher <lyyoicq@163.com>
2020-01-18 19:29:24 +0800

    10015 绝对值

---
+++ 10015/ac.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    double n ; 
+    cin >> n ;
+    if ( n < 0  ){
+        cout << -n << endl;
+    }
+    else {
+        cout << n << endl;
+    }
+    return 0;
+}
